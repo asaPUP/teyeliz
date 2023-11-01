6469d1997300c5a0e51c9c3e30e682789f1e0453
asaPUP <asael.silvam@gmail.com>
2023-11-01 10:19:24 -0700

    first

---
+++ teyeliz.py
@@ -0,0 +1,23 @@
+"""
+Facultad de Ciencias, UABC  | Semestre 2023-2
+Proyecto Final | Sistemas Operativos
+Dr. Everardo Gutiérrez López
+Alfredo Asael Silva Mirus     | 365393
+Valeria Isela Morales Romero  | 365418
+"""
+
+# Import Python modules
+import os
+import sys
+import pygame
+from random import randint
+from time import sleep
+from threading import Thread
+import socket
+
+# Constant definitions
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+TITLE = "Teyeliz"
+FONT = "fonts/PressStart2P-Regular.ttf"
